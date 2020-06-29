320d0a26ce7775790bea347f1a25c23e1fd3a76a
Carlos <carlos_agudo@hotmail.com>
2020-06-29 03:19:58 +0200

    main func init database

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/C-Agudo/ecomerce-ApiRest/internal/database"
 	"github.com/C-Agudo/ecomerce-ApiRest/internal/logs"
 
 )
+
+func main(){
+	_ = logs.InitLogger()
+
+	client := database.NewSqlClient(source: "root:root@/ecommerce")
+}
